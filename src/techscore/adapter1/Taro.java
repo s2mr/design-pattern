a229956b1c52e3c24c6c11964f5da46e456cd400
Kazumasa Shimomura <kazlv.1110@gmail.com>
2017-09-17 14:28:58 +0900

    adapter by using extends

---
+++ src/techscore/adapter1/Taro.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package techscore.adapter1;
+
+/**
+ *
+ * @author kaz
+ */
+public class Taro {
+
+    public void enjoyWithAllClassmates() {
+        System.out.println("みんなで楽しむ");
+    }
+}
