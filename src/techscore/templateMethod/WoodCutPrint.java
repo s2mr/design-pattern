0847b67f0dcf45ffa6eed56026dec6f708c4ad9b
kazu <g031n085@s.iwate-pu.ac.jp>
2017-09-18 14:48:55 +0000

    techscore.templatesMethod

---
+++ src/techscore/templateMethod/WoodCutPrint.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package techscore.templateMethod;
+
+/**
+ *
+ * @author kaz
+ */
+public abstract class WoodCutPrint {
+    
+    public abstract void draw(Cuttable hanzai);
+    
+    public abstract void cut(Cuttable hanzai);
+    
+    public abstract void print(Cuttable hanzai);
+    
+    public void createWoodCutPrint() {
+        Wood hanzai = new Wood();
+        draw(hanzai);
+        cut(hanzai);
+        print(hanzai);
+    }
+}
